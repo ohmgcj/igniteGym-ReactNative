6dd60d2a9a365dc54a7c7271fe547f423d867bba
Caio Alves <j4c.2@hotmail.com>
2024-10-08 23:55:42 -0300

    Rotas privadas criadas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
 import { Box } from '@gluestack-ui/themed'
 
 import { AuthRoutes } from './auth.routes'
+import { AppRoutes } from './app.routes'
 
 import { gluestackUIConfig } from '../../config/gluestack-ui.config'
 
@@ -11,7 +12,7 @@ export function Routes() {
     return(
         <Box flex={1} bg='$gray700'>
         <NavigationContainer theme={theme}>
-            <AuthRoutes/>
+            <AppRoutes />
         </NavigationContainer>
         </Box>
     )
