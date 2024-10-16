04c1dc8f8f2ae3e2c67fb49ae570823e6c3a11ad
Caio Alves <j4c.2@hotmail.com>
2024-10-16 01:30:35 -0300

    Image picker adicionado, agora é possível manipular imagens da galeria do dispositivo

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,23 +1,74 @@
 import { ScrollView, TouchableOpacity } from 'react-native'
-import { Center, Heading, Text, VStack } from "@gluestack-ui/themed";
+import { useState } from 'react';
+
+import { Center, Heading, Text, VStack, useToast } from "@gluestack-ui/themed";
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system'
 
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from '@components/UserPhoto';
 import { Input } from '@components/Input'
 import { Button } from '@components/Button';
+import { ToastMessage } from '@components/ToastMessage';
+import { IdCard } from 'lucide-react-native';
 
 export function Profile() {
+    const [userPhoto, setUserPhoto] = useState('https://github.com/ohmgcj.png')
+    
+    const toast = useToast()
+
+    async function handleUserPhotoSelect(){
+        try {
+            const photoSelected = await ImagePicker.launchImageLibraryAsync({
+                mediaTypes: ImagePicker.MediaTypeOptions.Images,
+                quality: 1,
+                aspect: [4, 4],
+                allowsEditing: true,
+            })
+    
+            if(photoSelected.canceled) {
+                return
+            }
+    
+            const photoURI = photoSelected.assets[0].uri
+    
+            if(photoURI){
+                const photoInfo = (await FileSystem.getInfoAsync(photoURI)) as { size: number };
+    
+                if(photoInfo.size && (photoInfo.size / 1024 / 1024) > 5){
+                    return toast.show({
+                        placement: 'top',
+                        render: ({ id }) => (
+                            <ToastMessage 
+                            id={id} 
+                            action='error'
+                            title='Imagem muito grande' 
+                            description='Essa imagem é muito grande' 
+                            onClose={() => toast.close(id)}
+                            ></ToastMessage>
+                        )
+                    })
+                }
+    
+                setUserPhoto(photoURI)
+            }          
+        } catch (error) {
+            console.log(error)
+        }
+
+            
+    }
     return(
         <VStack flex={1}>
             <ScreenHeader title='Perfil'/>
             <ScrollView contentContainerStyle={{ paddingBottom: 36}}>
                 <Center mt='$6' px='$10'>
                     <UserPhoto 
-                    source={{ uri: 'https://github.com/ohmgcj.png'}}
+                    source={{ uri: userPhoto}}
                     alt='Foto do usuário'
                     size='xl'
                     />
-                    <TouchableOpacity>
+                    <TouchableOpacity onPress={handleUserPhotoSelect}>
                         <Text 
                         color='$green500' 
                         fontFamily='$heading' 
